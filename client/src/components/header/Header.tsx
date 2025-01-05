90102a7dc57aeff81bfda4f3c26d9fdaf474f91d
DeclanLacey <declan.lacey@hotmail.com>
2025-01-05 17:16:13 -0600

    Header styling

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,19 +1,56 @@
 import { NavLink } from 'react-router'
+import { useState } from 'react'
+import "./BurgerMenu.css"
 import "./Header.css"
 
 export default function Header() {
+
+  const [burgerMenuOpen, setBurgerMenuOpen] = useState<boolean>(false)
+  const [hasInteracted, setHasInteracted] = useState(false);
+
+
+  function setActiveStyling(active : {isActive: boolean, isPending: boolean, isTransitioning: boolean}) {
+    if (active.isActive) {
+      return {textDecoration: "underline"}
+    }
+  }
+
+  function handleBurgerMenuClick(burgerMenuOpen : boolean, setBurgerMenuOpen: Function) {
+    setBurgerMenuOpen(!burgerMenuOpen)
+    setHasInteracted(true);
+
+  }
+
   return (
     <header className='header'>
+        <div className={`header_burger-menu ${burgerMenuOpen ? "open" : ""}`} onClick={() => handleBurgerMenuClick(burgerMenuOpen, setBurgerMenuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <img className='header_logo' src='./assets/symbol-transparent.png'/>
-        <nav className='header_nav'>
-            <NavLink className="header_nav-link" to="/catalog" > Catalog </NavLink>
-            <NavLink className="header_nav-link" to="/comparison" > Comparison </NavLink>
-            <NavLink className="header_nav-link" to="/" > Home </NavLink>
-            <NavLink className="header_nav-link" to="/collections" > Collections </NavLink>
-            <NavLink className="header_nav-link" to="/user/favorites" > Favorites </NavLink>
+
+        <nav className={`header_nav ${hasInteracted ? (burgerMenuOpen ? "header_nav-show" : "header_nav-hide") : ""}`}>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/catalog" > Catalog </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/comparison" > Comparison </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/" > Home </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/collections" > Collections </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/user/favorites" > Favorites </NavLink>
         </nav>
 
-        <img className='header_user-icon' src='./assets/user-icon.svg'/>
+        <nav className={`header_nav-desktop`}>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/catalog" > Catalog </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/comparison" > Comparison </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/" > Home </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/collections" > Collections </NavLink>
+            <NavLink className="header_nav-link" style={(state) => setActiveStyling(state)} to="/user/favorites" > Favorites </NavLink>
+        </nav>
+
+
+
+        <NavLink to="/user">
+          <img className='header_user-icon' src='./assets/user-icon.svg'/>
+        </NavLink>
     </header>
   )
 }
